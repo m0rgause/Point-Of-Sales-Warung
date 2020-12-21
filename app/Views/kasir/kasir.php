3da0564e3f263bff0c3b83b57808aeb8c6e3a294
fikkrireza <fikkri.reza@gmail.com>
2020-12-21 11:58:49 +0700

    continue make kasir page (change icon cart outline to fill, remove class bi bi-* in svg icon, make product_populer and product_more design, make loading and btn loading, add btn outline and disabled style, change naming mixin and variabel in scss file and add z-index 9 to header)

--- app/Views/kasir/kasir.php
+++ app/Views/kasir/kasir.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 
-<nav class="header container-xxl d-flex justify-content-between sticky-top">
+<nav class="header container-xxl d-flex justify-content-between">
     <ul class="header__left">
         <li><a href=""><img src="<?= base_url('/dist/images/posw.svg'); ?>" alt="posw logo" width="100"></a></li>
     </ul>
@@ -41,45 +41,115 @@
                         <input class="form-input" type="text" placeholder="Nama Barang...">
                         <div class="input-group__append">
                         <a class="btn" href="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/><path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/><path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/></svg>
                         </a>
                         </div>
                     </div><!-- input-group -->
                 </li>
                 <li><a href="">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
                 </a></li>
             </ul>
         </div><!-- product__filter -->
 
-        <h5>Produk Terlaris</h5>
+        <h5 class="mb-2">Produk Terlaris</h5>
         <div class="product__populer">
 
             <div class="product__item">
                 <div class="product__image">
                     <img src="<?= base_url('dist/images/apple [noupload].jpg'); ?>" alt="Apple">
                 </div>
-                <h6>Samsung J Prime Mini</h6>
-                <p>Handphone</p>
+                <div class="product__info">
+                    <p class="product__name">iPhone 12 Pro</p>
+                    <p class="product__category">Handphone</p>
+                    <p class="product__sales">Terjual 11</p>
 
-                <div class="product__price">
-                    <select name="besaran">
-                        <option value="">1 Buah</option>
-                    </select>
-                    <p>Rp 3.000</p>
+                    <div class="product__price">
+                        <h5>Rp 50.000</h5><span>/</span>
+                        <select name="besaran">
+                            <option value="">1 Buah</option>
+                        </select>
+                    </div>
                 </div>
-                <p class="product__sales">11 Terjual</p>
-
                 <div class="product__action">
-                    <input type="number" class="form-input">
+                    <input type="number" class="form-input" placeholder="Jumlah...">
                     <a class="btn" href="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
+                    </a>
+                </div>
+            </div><!-- product__item -->
+
+            <div class="product__item">
+                <div class="product__image">
+                    <img src="<?= base_url('dist/images/lensa [noupload].jpg'); ?>" alt="Lensa">
+                </div>
+                <div class="product__info">
+                    <p class="product__name">Lensa Fix 50mm Canon</p>
+                    <p class="product__category">Lensa</p>
+                    <p class="product__sales">Terjual 100</p>
+
+                    <div class="product__price">
+                        <h5>Rp 50.000</h5><span>/</span>
+                        <select name="besaran">
+                            <option value="">1 Buah</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="product__action">
+                    <input type="number" class="form-input" placeholder="Jumlah...">
+                    <a class="btn" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
                     </a>
                 </div>
             </div><!-- product__item -->
 
         </div><!-- product__populer -->
-    </div>
+
+        <h5 class="mb-2 mt-5">Produk Lainnya</h5>
+        <div class="product__more mb-5">
+
+            <div class="product__item">
+                <div class="product__image">
+                    <img src="<?= base_url('dist/images/handphone [noupload].jpg'); ?>" alt="Samsung">
+                </div>
+                <div class="product__info">
+                    <p class="product__name">Galaxy S20 FE</p>
+                    <p class="product__category">Handphone</p>
+                    <p class="product__sales">Terjual 5</p>
+
+                    <div class="product__price">
+                        <h5>Rp 3.249.000</h5><span>/</span>
+                        <select name="besaran">
+                            <option value="">1 Buah</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="product__action">
+                    <input type="number" class="form-input" placeholder="Jumlah...">
+                    <a class="btn" href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
+                    </a>
+                </div>
+            </div><!-- product__item -->
+
+        </div><!-- product__more -->
+
+        <div class="d-flex justify-content-center">
+            <div class="position-relative">
+                <a class="btn btn--blue-outline btn--disabled" href="">Lihat Lebih Banyak
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/><path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+</svg>
+                </a>
+
+                <div class="btn-loading d-flex justify-content-center align-items-center">
+                    <div class="loading">
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div><!-- product -->
 
     <aside class="cart d-none">
         <h5>Keranjang Belanja</h5>
