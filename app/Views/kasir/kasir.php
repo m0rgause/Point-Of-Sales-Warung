409416eed22679e9992c0655f544ae0b218df30f
fikkrireza <fikkri.reza@gmail.com>
2020-12-21 21:33:09 +0700

    continue make kasir page (make product_image_zoom)

--- app/Views/kasir/kasir.php
+++ app/Views/kasir/kasir.php
@@ -58,6 +58,7 @@
             <div class="product__item">
                 <div class="product__image">
                     <img src="<?= base_url('dist/images/apple [noupload].jpg'); ?>" alt="Apple">
+                    <a class="btn btn--close d-none" href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                 </div>
                 <div class="product__info">
                     <p class="product__name">iPhone 12 Pro</p>
@@ -82,6 +83,7 @@
             <div class="product__item">
                 <div class="product__image">
                     <img src="<?= base_url('dist/images/lensa [noupload].jpg'); ?>" alt="Lensa">
+                    <a class="btn btn--close d-none" href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                 </div>
                 <div class="product__info">
                     <p class="product__name">Lensa Fix 50mm Canon</p>
@@ -111,6 +113,7 @@
             <div class="product__item">
                 <div class="product__image">
                     <img src="<?= base_url('dist/images/handphone [noupload].jpg'); ?>" alt="Samsung">
+                    <a class="btn btn--close d-none" href=""><svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></a>
                 </div>
                 <div class="product__info">
                     <p class="product__name">Galaxy S20 FE</p>
--- public/dist/js/posw.js
+++ public/dist/js/posw.js
@@ -3,6 +3,7 @@
  * Licensed under GPL (https://github.com/rezafikkri/Point-Of-Sales-Warung/blob/master/LICENSE)
  */
 
+// dropdown menu
 const nav = document.querySelector("nav.header");
 nav.addEventListener('click', e => {
     const target = e.target;
@@ -14,5 +15,25 @@ nav.addEventListener('click', e => {
         dropdown_menu.classList.toggle('d-none');
         dropdown_menu.parentElement.classList.toggle('dropdown--active');
 
+    }
+});
+
+// product image zoom
+const product = document.querySelector('div.product');
+product.addEventListener('click', (e) => {
+    let target = e.target;
+    if(!target.classList.contains('product__image')) target = target.parentElement;
+
+    if(target.classList.contains('product__image')) {
+        target.classList.add('product__image--zoom');
+        target.querySelector('.btn--close').classList.remove('d-none');
+    }
+
+    if(!target.classList.contains('btn--close')) target = target.parentElement;
+
+    if(target.classList.contains('btn--close')) {
+        e.preventDefault();
+        target.parentElement.classList.remove('product__image--zoom');
+        target.classList.add('d-none');
     }
 });
